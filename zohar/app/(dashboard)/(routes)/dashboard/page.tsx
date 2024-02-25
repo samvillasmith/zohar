c1af3aa0113ef0ab7f4b5ca39023d33ae1fa4e04
samvillasmith <samuel.villa.smith@hotmail.com>
2024-02-25 14:29:53 -0600

    built out route components

--- zohar/app/(dashboard)/(routes)/dashboard/page.tsx
+++ zohar/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,73 @@
+"use client";
+
+import { Code, Image, MessageSquare, Music, Video } from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils"
+
+
+const tools = [
+  {
+    label: "Conversational Chatbot",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/chatbot",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-blue-500",
+    bgColor: "bg-blue-500/10",
+    href: "/code",
+  },
+  {
+    label: "Image Generation",
+    icon: Image,
+    color: "text-green-500",
+    bgColor: "bg-green-500/10",
+    href: "/image",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-yellow-500",
+    bgColor: "bg-yellow-500/10",
+    href: "/music",
+  },
+  {
+    label: "Video Generation",
+    icon: Video,
+    color: "text-red-500",
+    bgColor: "bg-red-500/10",
+    href: "/video",
+  }
+]
+
 const DashboardPage = () => {
   return (
-  <div>
-   <p>Dashboard (Protected)</p>
+  <div className="mb-8 space-y-4">
+    <h2 className="text-2xl md:text-4xl font-bold text-center">
+      Explore the Power of AI
+    </h2>
+    <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+      Zohar is a powerful all-in-one AI platform that helps provides you with a chatbot advisor and all the capabilities of AI generation ranging from code and images to music and video.
+    </p>
+    <div className="px-4 md:px-20 lg:px-32 space-y-4">
+      {tools.map((tool)=>(
+        <Card
+          key={tool.href}
+          className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+                </div>
+                <div className="font-semibold">
+                  {tool.label}
+                </div>
+              </div>
+        </Card>
+      ))}
+    </div>
   </div>
   );
 }
--- zohar/components/sidebar.tsx
+++ zohar/components/sidebar.tsx
@@ -11,6 +11,7 @@ import {
     Music, 
     Settings, 
     VideoIcon } from "lucide-react";
+import { usePathname } from 'next/navigation';
 
 import { cn } from "@/lib/utils";
 
@@ -64,6 +65,7 @@ const routes = [
 ]
 
 const Sidebar = () => {
+    const pathname = usePathname();
     return ( 
         <div className="space-y-4 py-5 flex flex-col h-full bg-[#030a18] text-white">
             <div className="px-3 py-5 flex-1">
@@ -84,7 +86,8 @@ const Sidebar = () => {
                         <Link 
                         href={route.href} 
                         key={route.href}
-                        className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+                        className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", 
+                        pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}
                         >
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
