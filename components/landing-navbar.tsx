8fe5c3ca3b67a58c3c3e916cf863f8a150b92f48
samvillasmith <samuel.villa.smith@hotmail.com>
2024-03-08 23:22:29 -0600

    fixing linting errors

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -31,7 +31,7 @@ export const LandingNavbar =()=>{
                 </div>
             </Link>
             <div className="flex items-center gap-x-2">
-                <Link href={isSignedIn ? "/dashboard" : "/signup"}>
+                <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
                     <Button variant="outline" className="rounded-full">
                         Get Started
                     </Button>
